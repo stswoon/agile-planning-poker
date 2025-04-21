bbe29d4852cb3bee241897898f194fc2a755b125
stswoon <xx@xx.xx>
2025-04-21 20:40:46 +0400

    MUI + cache

--- backend/src/utils/staticFilesCacheHeaders.util.ts
+++ backend/src/utils/staticFilesCacheHeaders.util.ts
@@ -1,9 +1,17 @@
 import path from "node:path";
 import { Response } from "express";
 
+const h1 = 60 * 60 * 1000;
+const d1 = 24 * h1;
+const d365 = 365 * d1;
+
 function setStaticFilesCacheHeaders(res: Response, file: string) {
     if (path.extname(file) !== ".html") {
-        res.setHeader("Cache-Control", "public, max-age=31557600");
+        let maxAge = d365;
+        if (path.extname(file) !== "favicon.svg") {
+            maxAge = d1;
+        }
+        res.setHeader("Cache-Control", `public, max-age=${maxAge}`);
     }
 }
 
