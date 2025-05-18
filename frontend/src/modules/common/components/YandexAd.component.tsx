c08722b91b48d2b249c30de1e38d2d1b0b0e6071
stswoon <xx@xx.xx>
2025-05-18 21:07:18 +0400

    new donate

--- frontend/src/modules/common/components/YandexAd.component.tsx
+++ frontend/src/modules/common/components/YandexAd.component.tsx
@@ -2,17 +2,16 @@ import { FC, memo, useEffect } from "react";
 
 const YandexAd: FC = memo(() => {
     useEffect(() => {
-        // Ensure the global callback array exists
         window.yaContextCb = window.yaContextCb || [];
         window.yaContextCb.push(() => {
             window.Ya.Context.AdvManager.render({
-                blockId: "R-A-15348165-1",
-                renderTo: "yandex_rtb_R-A-15348165-1",
+                blockId: "R-A-15536913-1",
+                renderTo: "yandex_rtb_R-A-15536913-1",
             });
         });
     }, []);
 
-    return <div className="taYandexAd" id="yandex_rtb_R-A-15348165-1"></div>;
+    return <div className="taYandexAd" id="yandex_rtb_R-A-15536913-1"></div>;
 });
 
 export { YandexAd };
